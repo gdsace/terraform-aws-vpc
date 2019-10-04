005dbc0967d3d66e14940be35abcc7cde34b3dfd
Curtis Tan <merwinwee@gmail.com>
2019-10-04 13:12:16 +0800

    Tentatively upgrade terraform-aws-vpc version

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_eip" "nat" {
 }
 
 module "vpc" {
-  source = "github.com/terraform-aws-modules/terraform-aws-vpc?ref=v1.41.0"
+  source = "github.com/terraform-aws-modules/terraform-aws-vpc?ref=v2.17.0"
 
   name = var.vpc_name
   cidr = var.vpc_cidr
